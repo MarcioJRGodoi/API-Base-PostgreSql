ab17a79906dcee4e421932d177ac29432afb9c8e
Marcio.Godoi <marciojrgodoi@gmail.com>
2023-09-06 13:42:36 -0300

    modificadas algumas partes para serem variaveis de ambiente

--- API_PostgreSql/Infrastructure/ConnectionDatabase.cs
+++ API_PostgreSql/Infrastructure/ConnectionDatabase.cs
@@ -10,11 +10,6 @@ namespace API_PostgreSql.Infrastructure
         public DbSet<Cage> Cages { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-                optionsBuilder.UseNpgsql(
-                "Server=silly.db.elephantsql.com;" +
-                "Port=5432;" +
-                "Database=yczascmf;" +
-                "User Id=yczascmf;" +
-                "Password=yrU9L_O6fDkPATpwhDM1O3Rg1npQ_94M;");
+                optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
     }
 }
