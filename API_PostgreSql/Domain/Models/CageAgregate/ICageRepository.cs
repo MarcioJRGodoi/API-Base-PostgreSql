e5f78e51593643f875d25158401ae839d91ea698
Mateus Soares <mateusviniciussoaresdasilva@gmail.com>
2023-09-11 22:59:46 -0300

    adicionado DELETE no CageController.cs

--- API_PostgreSql/Domain/Models/CageAgregate/ICageRepository.cs
+++ API_PostgreSql/Domain/Models/CageAgregate/ICageRepository.cs
@@ -8,6 +8,6 @@ namespace API_PostgreSql.Domain.Models.CageAgregate
         Task<List<CageDTO>> GetAll();
         Task<Cage> Get(int id);
         Task<bool> Update(int id, Cage cage);
-        Task<Cage> Delete(int id);
+        Task<bool> Delete(int id);
     }
 }
